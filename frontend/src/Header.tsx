ad6b6124f85ffad917492a9e0ad6631decf90fdd
Amber <woll.ambermadison@gmail.com>
2025-03-30 13:29:19 -0600

    more descriptive header

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,5 +1,13 @@
 function Header() {
-  return <h1>Bowlers on Marlins and Sharks</h1>;
+  return (
+    <>
+      <h1>Bowlers on Marlins and Sharks</h1>
+      <h4>
+        Below is a list of the bowling league members and their information on
+        the teams the Marlins and the Sharks.
+      </h4>
+    </>
+  );
 }
 
 export default Header;
